7ae62f5128fe4d9c05070e224cc9beb3851d970f
SylviaLi-Y <neerGGreen@live.com>
2015-01-24 20:17:49 +0800

    好吧这个才是基类

---
+++ Closet/Closet/Closet.Shared/PictureItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Closet
+{
+    class PictureItem
+    {
+        private string picName;
+
+        public string PicName
+        {
+            get { return picName; }
+            set { picName = value; }
+        }
+
+        private string[] picTags = new string[5];
+
+        public string[] PicTags
+        {
+            get { return picTags; }
+            set { picTags = value; }
+        }
+
+        
+        private bool isPublish;
+
+        public bool IsPublish
+        {
+            get { return isPublish; }
+            set { isPublish = value; }
+        }
+
+    }
+}
